9438fd20a3a629bcc0c525aa9f550255c0c4b2f8
tlr24 <tlr24@njit.edu>
2022-03-31 21:00:57 -0400

    Updated some test comments

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -41,8 +41,7 @@ def test_successful_login(client):
     with client.application.app_context():
         user_id = User.query.filter_by(email="a@a.com").first().get_id()
 
-    # login request set the user_id in the session
-    # check that the user is loaded from the session
+    # check that the login request set the user_id in the session
     with client:
         client.get("/")
         assert session["_user_id"] == user_id
@@ -110,6 +109,7 @@ def test_allowing_dashboard_access(client):
 def test_logout(client):
     """Testing logging out"""
     client.post("/login", data={"email": "a@a.com", "password": "12345678"}, follow_redirects=True)
+    # check that the user_id in the session is removed
     with client:
         client.get("/logout")
         assert "_user_id" not in session
